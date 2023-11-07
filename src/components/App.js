965f429d26482c405ef9f9e22bee74b33619d108
Kamauleonard <leonardmwangi2012@gmail.com>
2023-11-07 18:02:48 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Navbar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import { username, city, image } from "../data/user";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <Navbar />
       <Home />
       <About />
+      <div>
+        <p>Username: {username}</p>
+        <p>City: {city}</p>
+        <img src={image} alt="User's Image" />
+      </div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function NavBar() {
+function Navbar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
   );
 }
+
+export default Navbar;
